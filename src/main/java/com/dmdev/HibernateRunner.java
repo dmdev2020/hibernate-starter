64d94b6739e80b5705dbd3d40d634541753cdd2f
dmatveyenka <dmdev2020@gmail.com>
2021-08-28 12:43:06 +0200

    added files from lesson-11

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -5,6 +5,7 @@ import com.dmdev.entity.User;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
@@ -12,16 +13,9 @@ import java.sql.SQLException;
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
-//        BlockingDeque<Connection> pool = null;
-//        Connection connection = pool.take();
-//        SessionFactory
-
-//        Connection connection = DriverManager
-//                .getConnection("db.url", "db.username", "db.password");
-//        Session
         Configuration configuration = new Configuration();
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-//        configuration.addAnnotatedClass(User.class);
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAnnotatedClass(User.class);
         configuration.addAttributeConverter(new BirthdayConverter());
         configuration.registerTypeOverride(new JsonBinaryType());
         configuration.configure();
