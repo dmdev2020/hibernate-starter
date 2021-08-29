16a3eb80ceaaff6dd0aefbd7da4769bc3d936dbb
dmatveyenka <dmdev2020@gmail.com>
2021-08-29 16:53:05 +0200

    added files from lesson-18

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -2,17 +2,15 @@ package com.dmdev;
 
 import com.dmdev.entity.User;
 import com.dmdev.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class HibernateRunner {
-
-    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
 
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
