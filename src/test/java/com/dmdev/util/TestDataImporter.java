e24e110dc039025738b344bade671a86390ba27c
dmdev2020 <dmdev2020@gmail.com>
2021-10-09 11:01:05 +0200

    preparation for lesson 54

--- src/test/java/com/dmdev/util/TestDataImporter.java
+++ src/test/java/com/dmdev/util/TestDataImporter.java
@@ -1,9 +1,11 @@
 package com.dmdev.util;
 
+import com.dmdev.entity.Chat;
 import com.dmdev.entity.Company;
 import com.dmdev.entity.Payment;
 import com.dmdev.entity.PersonalInfo;
 import com.dmdev.entity.User;
+import com.dmdev.entity.UserChat;
 import lombok.Cleanup;
 import lombok.experimental.UtilityClass;
 import org.hibernate.Session;
@@ -11,6 +13,7 @@ import org.hibernate.SessionFactory;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Arrays;
 
 @UtilityClass
 public class TestDataImporter {
@@ -51,6 +54,32 @@ public class TestDataImporter {
         savePayment(session, dianeGreene, 300);
         savePayment(session, dianeGreene, 300);
         savePayment(session, dianeGreene, 300);
+
+        Chat dmdev = saveChat(session, "dmdev");
+        Chat java = saveChat(session, "java");
+        Chat youtubeMembers = saveChat(session, "youtube-members");
+
+        addToChat(session, dmdev, billGates, steveJobs, sergeyBrin);
+        addToChat(session, java, billGates, steveJobs, timCook, dianeGreene);
+        addToChat(session, youtubeMembers, billGates, steveJobs, timCook, dianeGreene);
+    }
+
+    private void addToChat(Session session, Chat chat, User... users) {
+        Arrays.stream(users)
+                .map(user -> UserChat.builder()
+                        .chat(chat)
+                        .user(user)
+                        .build())
+                .forEach(session::save);
+    }
+
+    private Chat saveChat(Session session, String chatName) {
+        Chat chat = Chat.builder()
+                .name(chatName)
+                .build();
+        session.save(chat);
+
+        return chat;
     }
 
     private Company saveCompany(Session session, String name) {
