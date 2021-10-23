3369671aa3837a2f3b0e8254564113a22154693e
dmatveyenka <dmdev2020@gmail.com>
2021-10-23 17:34:59 +0200

    added files from lesson-75

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -3,14 +3,10 @@ package com.dmdev;
 import com.dmdev.entity.Payment;
 import com.dmdev.util.HibernateUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.ReplicationMode;
 import org.hibernate.SessionFactory;
-import org.hibernate.envers.AuditReaderFactory;
-import org.hibernate.envers.query.AuditEntity;
 
 import javax.transaction.Transactional;
 import java.sql.SQLException;
-import java.util.Date;
 
 @Slf4j
 public class HibernateRunner {
@@ -26,24 +22,6 @@ public class HibernateRunner {
                 payment.setAmount(payment.getAmount() + 10);
 
                 session.getTransaction().commit();
-            }
-            try (var session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                var auditReader = AuditReaderFactory.get(session2);
-//                auditReader.find(Payment.class, 1L, 1L)
-                var oldPayment = auditReader.find(Payment.class, 1L, new Date(1635000657066L));
-                session2.replicate(oldPayment, ReplicationMode.OVERWRITE);
-
-                auditReader.createQuery()
-                        .forEntitiesAtRevision(Payment.class, 400L)
-                        .add(AuditEntity.property("amount").ge(450))
-                        .add(AuditEntity.property("id").ge(6L))
-                        .addProjection(AuditEntity.property("amount"))
-                        .addProjection(AuditEntity.id())
-                        .getResultList();
-
-                session2.getTransaction().commit();
             }
         }
     }
